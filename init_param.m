5564633410467c82c99dd4b98eb15d14d76df263
Atsushi Saitoh <>
2021-04-26 22:54:37 +0900

    update param

--- init_param.m
+++ init_param.m
@@ -1,12 +1,12 @@
 function params = init_param()
 
-W1 = 1/sqrt(3*3*2)*randn(3, 3, 3, 16); %FH,FW,C,FN
-b1 = zeros(1,1,16);
-W2 = 1/sqrt(3*3*2)*randn(3, 3, 16, 3);
+W1 = 1/sqrt(3*3*2)*randn(3, 3, 3, 32); %FH,FW,C,FN
+b1 = zeros(1,1,32);
+W2 = 1/sqrt(3*3*2)*randn(3, 3, 32, 3);
 b2 = 0;
-W3 = 1/sqrt(3*3*1)*randn(3, 3, 3, 8);
+W3 = 1/sqrt(3*3*1)*randn(3, 3, 3, 64);
 b3 = 0;
-W4 = 1/sqrt(128*500)*randn(128,5000);
+W4 = 1/sqrt(128*40000)*randn(128,40000);
 b4 = zeros(128, 1);
 W5 = 1/sqrt(10*128)*randn(10,128);
 b5 = zeros(10,1);
