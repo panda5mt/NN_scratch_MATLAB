275620e7444f76c0a72f2c5f6c3876455ca66737
Atsushi Saitoh <>
2021-04-26 15:49:17 +0900

    fixed matrix

--- init_param.m
+++ init_param.m
@@ -1,12 +1,12 @@
 function params = init_param()
 
-W1 = 1/sqrt(3*3*2)*randn(3, 3, 3, 64); %FH,FW,C,FN
-b1 = zeros(1,1,64);
-W2 = 1/sqrt(3*3*2)*randn(3, 3, 64, 3);
+W1 = 1/sqrt(3*3*2)*randn(3, 3, 3, 16); %FH,FW,C,FN
+b1 = zeros(1,1,16);
+W2 = 1/sqrt(3*3*2)*randn(3, 3, 16, 3);
 b2 = 0;
-W3 = 1/sqrt(3*3*1)*randn(3, 3, 3, 64);
+W3 = 1/sqrt(3*3*1)*randn(3, 3, 3, 16);
 b3 = 0;
-W4 = 1/sqrt(128*40000)*randn(128,40000);
+W4 = 1/sqrt(128*10000)*randn(128,10000);
 b4 = zeros(128, 1);
 W5 = 1/sqrt(10*128)*randn(10,128);
 b5 = zeros(10,1);
